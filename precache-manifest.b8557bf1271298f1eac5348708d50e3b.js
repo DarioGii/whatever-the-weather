d01a9940e70015f838eed94e86a0c59ca9c998ee
Dario Ghunney Ware <username@users.noreply.github.com>
2019-11-04 10:23:48 +0000

    Updates

---
+++ precache-manifest.b8557bf1271298f1eac5348708d50e3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b922af86511f18b438f6c6bb5b85b57",
+    "url": "/whatever-the-weather/index.html"
+  },
+  {
+    "revision": "819759c5081ee750c046",
+    "url": "/whatever-the-weather/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "819759c5081ee750c046",
+    "url": "/whatever-the-weather/static/js/2.60fab531.chunk.js"
+  },
+  {
+    "revision": "e51260dd3d9e07283c5d",
+    "url": "/whatever-the-weather/static/js/main.07b2a52a.chunk.js"
+  },
+  {
+    "revision": "e3925f3e2ffd06ed017d",
+    "url": "/whatever-the-weather/static/js/runtime~main.9d2b34cc.js"
+  }
+]);
